85d740030a325b2e2b10daa8753a57fe36cb17a9
guohai <guohai@siro-info.com>
2022-01-20 15:56:06 +0800

    工具类，统一配置类

---
+++ common/common-util/src/main/java/com/siro/common/util/BeanUtils.java
@@ -0,0 +1,9 @@
+package com.siro.common.util;
+
+/**
+ * 实体bean copy工具类
+ * @author starsea
+ * @date 2022-01-19
+ */
+public class BeanUtils {
+}
---
+++ common/service-util/src/main/java/com/siro/common/config/RedisConfig.java
@@ -0,0 +1,10 @@
+package com.siro.common.config;
+
+/**
+ * Redis配置类
+ * @author starsea
+ * @date 2022-01-20
+ */
+public class RedisConfig {
+
+}
